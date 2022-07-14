7ce7766b2025db02313e8b576cd1c91e2b96ae5d
shohrux <shohruxergashev67@gmail.com>
2022-07-14 10:41:41 +0500

    arthmetic

--- bin/arithmetic004.dart
+++ bin/arithmetic004.dart
@@ -1,4 +1,11 @@
 // Create function func with arguments
 // the task condition is in the Readme file
 
-void main() {}
+int func(int a, int b) {
+  return a ~/ b;
+}
+
+void main() {
+  print(func(6, 5));
+}
+

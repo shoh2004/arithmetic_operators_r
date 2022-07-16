2ec31dff5996faab1a09626ce3650478b0f124a8
shohrux <shohruxergashev67@gmail.com>
2022-07-16 10:34:45 +0500

    arithmetic

--- bin/arithmetic032.dart
+++ bin/arithmetic032.dart
@@ -1,4 +1,11 @@
 // Create function func with arguments
 // the task condition is in the Readme file
+import 'dart:math';
+num func(num a,num b,num c){
+    return ((a+b)/2)*(c/2);
+}
+void main(){
 
-void main() {}
+    print(func(1,11,4));
+}
+
